cfac35b1c2499705aad9df49f6eac6ff80f70674
Marcos Wilson Sarinho Brito <mwbrito@gmail.com>
2023-06-10 13:35:49 -0300

    acerto repository id

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,6 +1,6 @@
 resource "google_artifact_registry_repository" "my-repo" {
   location = var.region
-  repository_id = "labdevops"
+  repository_id = "labdevops-experience"
   description = "Imagens Docker"
   format = "DOCKER"
 }
