9d6e2d8cd4ba86ad2754f625e4769e0d43e500b9
Marcos Wilson Sarinho Brito <mwbrito@gmail.com>
2023-06-13 22:09:10 -0300

    entrega_solution_sprint

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,6 +1,6 @@
 resource "google_artifact_registry_repository" "my-repo" {
   location = var.region
   repository_id = "labdevops-experience"
-  description = "Imagens Docker"
+  description = "Entrega Final - Solution Sprint"
   format = "DOCKER"
 }
